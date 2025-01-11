b7ba5b5722632d16a77820cb1bae9ffa399ab85c
MarkiMark01 <markishgroup@gmail.com>
2025-01-11 16:49:03 +0100

    UserInfo

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,11 @@
 import "./list.css";
 import ChatList from "./chatList/ChatList";
+import UserInfo from "./userInfo/UserInfo";
 
 const List = () => {
   return (
     <section className="list">
+      <UserInfo />
       <ChatList />
     </section>
   );
---
+++ src/components/list/userInfo/UserInfo.jsx
@@ -0,0 +1,10 @@
+import "./userInfo.css";
+
+const UserInfo = () => {
+  return (
+    <section className="userInfo">
+      <h1>UserInfo</h1>
+    </section>
+  );
+};
+export default UserInfo;
