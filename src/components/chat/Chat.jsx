d36343eb3d893ebf8a39c8ad5dc85f9d71d45a06
MarkiMark01 <markishgroup@gmail.com>
2025-01-12 19:11:10 +0100

    created chat.css

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,10 +5,12 @@ const Chat = () => {
     <section className="chat">
       <div className="top">
         <div className="user">
-          <img src="./avatar.png" alt="Avatar" />
+          <img src="./avatar.png" alt="Avatar" className="imgAva" />
           <div className="texts">
-            <span>John Doe</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <span className="firstname">John Doe</span>
+            <p className="text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
         </div>
         <div className="icons">
@@ -18,7 +20,22 @@ const Chat = () => {
         </div>
       </div>
       <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="bottomIcons">
+          <img src="./img.png" alt="img" className="bottomIcons_img" />
+          <img src="./camera.png" alt="camera" className="bottomIcons_img" />
+          <img src="./mic.png" alt="mic" className="bottomIcons_img" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          className="bottomInput"
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="Emoji" />
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </section>
   );
 };
