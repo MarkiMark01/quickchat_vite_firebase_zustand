b8bb541614642866aa918eb28797e047203d82af
MarkiMark01 <markishgroup@gmail.com>
2025-01-12 17:30:52 +0100

    updated Chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,25 @@
-import './chat.css'
+import "./chat.css";
 
 const Chat = () => {
-return (
+  return (
     <section className="chat">
-        <h1>Chat</h1>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="Avatar" />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="Phone" />
+          <img src="./video.png" alt="Video" />
+          <img src="./info.png" alt="Info" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
     </section>
-)
-}
+  );
+};
 export default Chat;
